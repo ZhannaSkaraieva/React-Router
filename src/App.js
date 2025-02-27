844ad5b47743a0b2d2945c67482f16bd9bfb083f
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-02-28 00:47:07 +0200

    1.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-
+import React from 'react';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import { NavLink } from "react-router-dom";
 
 function App() {
-  return (
-    <div >
-          Learn React
-    </div>
-  );
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/" >Головна</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about" >Про нас</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact">Контакт</NavLink>
+                    </li>
+                </ul>
+            </nav>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1>404 - Сторінка не знайдена</h1>} />
+        </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export function Home () {
+    return (
+        <div>
+        <h1>Це домашня сторінка</h1>
+        </div>
+    );
+    }
+
+export default Home;
