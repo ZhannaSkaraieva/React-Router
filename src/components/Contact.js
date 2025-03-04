19a849b7c39060ab65edec35889bdb8d121938b0
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-03-05 00:24:36 +0200

    useNavigate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,15 @@
 import React from "react";  
+import { useNavigate } from "react-router";
+import './Contact.css';
 
 export function Contact() {
+    const navigate = useNavigate();
     return (
-        <div>
-        <h1>Це сторінка контактів</h1>
+        <div className="contact">
+            <h1>Це сторінка контактів</h1>
+            <button onClick={() => navigate("/")}>На головну</button>
         </div>
+        
     );
 }
 
